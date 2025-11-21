c306d55681cdc0e16e76f8204b9cb89c340c6aa3
femce4l20 <degendegeb@gmail.com>
2025-11-21 12:21:23 -0500

    Update tabs.lua

--- tabs.lua
+++ tabs.lua
@@ -1,89 +1,70 @@
 return {
-    {
-        name = "nsfw/m",
-        animations = {
-            { id = "116268945019667", name = "bang ur head" },
-            { id = "83879434467976", name = "boing bottom", start = nil, finish = nil, hipHeight = -0.2 },
-            { id = "125136786273414", name = "die lit" },
-            { id = "115812402435199", name = "lower torso bang" },
-            { id = "86745448648267", name = "OGGGGGGGGG" },
-            { id = "131061459597861", name = "RunningManBang" },
-            { id = "107639100736436", name = "slow slide" },
-            { id = "99637983789946", name = "Sus jig" },
-            { id = "78093637798125", name = "Watch it bounce", start = 1.5, finish = 8, hipHeight = -0.3 }
-        }
-    },
-    {
-        name = "nsfw/f",
-        animations = {
-            { id = "127274457723850", name = "Back Thrust", start = nil, finish = nil, hipHeight = -0.5 },
-            { id = "137291954460051", name = "bend 1" },
-            { id = "71117297875716", name = "bend 2" },
-            { id = "74919246969815", name = "bend 3", start = nil, finish = nil, hipHeight = -0.5 },
-            { id = "87452671760125", name = "bent standing" },
-            { id = "122937877015539", name = "bent standing2" },
-            { id = "87272067762119", name = "bouncy" },
-            { id = "84835619003157", name = "boing top" },
-            { id = "125965188125293", name = "handjob" },
-            { id = "92811207602426", name = "Jack-o" },
-            { id = "124575754112740", name = "Rub/Finger" },
-            { id = "107301013061641", name = "open yer legs", start = nil, finish = nil, hipHeight = -0.5 },
-            { id = "134343193928632", name = "on back", start = 0.999, finish = 1, hipHeight = -0.5 },
-            { id = "103699000387669", name = "legs up" },
-            { id = "103065929051231", name = "succ1" },
-            { id = "124873747842579", name = "succ2" },
-            { id = "85478341360173", name = "succ3", start = 1, finish = 10 },
-            { id = "81284540778909", name = "succ4/bend" },
-            { id = "86519407452355", name = "Serious hip movement" }
-        }
-    },
-    {
-        name = "sfw/m",
-        animations = {
-            { id = "85638447484999", name = "sit hug guy" },
-            { id = "105341373580564", name = "match sit 2" },
-            { id = "105824857171709", name = "stand hug" }
-        }
-    },
-    {
-        name = "sfw/f",
-        animations = {
-            { id = "71604609626894", name = "new cuddle grl" },
-            { id = "96867341841188", name = "cuddles", hipHeight = -1 },
-            { id = "104687069461693", name = "cuddles2" },
-            { id = "108208044953488", name = "cuddles3", hipHeight = -3 },
-            { id = "101127053189586", name = "cuddles4", hipHeight = -0.2 },
-            { id = "108360859241335", name = "mwamwamwamwa" },
-            { id = "75399969647635", name = "leg up hug" },
-            { id = "78664527797699", name = "sit hug girl" },
-            { id = "95980035499617", name = "match sit 1" },
-            { id = "105824857171709", name = "stand hug" },
-            { id = "72135287204792", name = "hips sway shy" },
-            { id = "104660720214070", name = "hips sway" },
-            { id = "122545939134570", name = "crossarms hipsway" },
-            { id = "138485791735842", name = "hips sway cute" },
-            { id = "72135287204792", name = "dancing waist1" }
-        }
-    },
-    {
-        name = "furry",
-        animations = {
-            { id = "106938941181821", name = "tail wag" },
-            { id = "129026910898635", name = "tail wag2" },
-            { id = "90254934897888", name = "kitty dance :3" },
-            { id = "138954156455492", name = "pawsies :3" },
-            { id = "104452303969102", name = "kitty pose :3" }
-        }
-    },
-    {
-        name = "misc",
-        animations = {
-            { id = "101385394794634", name = "Feeling pretty" },
-            { id = "138316142522795", name = "hip sway" },
-            { id = "109364514498221", name = "AppleStore" },
-            { id = "96720886821465", name = "oops!" },
-            { id = "77780134372141", name = "feelin myself sway" },
-            { id = "88038962894440", name = "PhutHon" }
-        }
-    }
+  { name = "nsfw/m", animations = {
+    { id = "116268945019667", name = "bang ur head" },
+    { id = "83879434467976", name = "boing bottom", hipHeight = -0.2 },
+    { id = "125136786273414", name = "die lit" },
+    { id = "115812402435199", name = "lower torso bang" },
+    { id = "86745448648267", name = "OGGGGGGGGG" },
+    { id = "131061459597861", name = "RunningManBang" },
+    { id = "107639100736436", name = "slow slide" },
+    { id = "99637983789946", name = "Sus jig" },
+    { id = "78093637798125", name = "Watch it bounce", start = 1.5, finish = 8, hipHeight = -0.3 },
+    { id = "rbxassetid://71576547132407", name = "MJ bang", start = .99, finish = 5.80 },
+  } },
+  { name = "nsfw/f", animations = {
+    { id = "127274457723850", name = "Back Thrust", hipHeight = -0.5 },
+    { id = "137291954460051", name = "bend 1" },
+    { id = "71117297875716", name = "bend 2" },
+    { id = "74919246969815", name = "bend 3", hipHeight = -0.5 },
+    { id = "87452671760125", name = "bent standing" },
+    { id = "122937877015539", name = "bent standing2" },
+    { id = "87272067762119", name = "bouncy" },
+    { id = "84835619003157", name = "boing top" },
+    { id = "125965188125293", name = "handjob" },
+    { id = "92811207602426", name = "Jack-o" },
+    { id = "124575754112740", name = "Rub/Finger" },
+    { id = "107301013061641", name = "open yer legs", hipHeight = -0.5 },
+    { id = "134343193928632", name = "on back", start = 0.999, finish = 1, hipHeight = -0.5 },
+    { id = "103699000387669", name = "legs up" },
+    { id = "103065929051231", name = "succ1" },
+    { id = "124873747842579", name = "succ2" },
+    { id = "85478341360173", name = "succ3", start = 1, finish = 10 },
+    { id = "81284540778909", name = "succ4/bend" },
+    { id = "rbxassetid://71970944840848", name = "hula" },
+  } },
+  { name = "sfw/m", animations = {
+    { id = "85638447484999", name = "sit hug guy" },
+    { id = "105341373580564", name = "match sit 2" },
+    { id = "105824857171709", name = "stand hug" },
+  } },
+  { name = "sfw/f", animations = {
+    { id = "71604609626894", name = "new cuddle grl" },
+    { id = "96867341841188", name = "cuddles", hipHeight = -1 },
+    { id = "104687069461693", name = "cuddles2" },
+    { id = "108208044953488", name = "cuddles3", hipHeight = -3 },
+    { id = "101127053189586", name = "cuddles4", hipHeight = -0.2 },
+    { id = "108360859241335", name = "mwamwamwamwa" },
+    { id = "75399969647635", name = "leg up hug" },
+    { id = "78664527797699", name = "sit hug girl" },
+    { id = "95980035499617", name = "match sit 1" },
+    { id = "105824857171709", name = "stand hug" },
+    { id = "rbxassetid://119542856256067", name = "Clingy Hugging Sit" },
+  } },
+  { name = "furry", animations = {
+    { id = "106938941181821", name = "tail wag" },
+    { id = "129026910898635", name = "tail wag2" },
+    { id = "90254934897888", name = "kitty dance :3" },
+    { id = "138954156455492", name = "pawsies :3" },
+    { id = "104452303969102", name = "kitty pose :3" },
+    { id = "rbxassetid://1", name = "test" },
+    { id = "rbxassetid://111892394639126", name = "lick leg cat" },
+  } },
+  { name = "misc", animations = {
+    { id = "101385394794634", name = "Feeling pretty" },
+    { id = "138316142522795", name = "hip sway" },
+    { id = "109364514498221", name = "AppleStore" },
+    { id = "96720886821465", name = "oops!" },
+    { id = "rbxassetid://138256733896316", name = "Rosa Shake" },
+    { id = "rbxassetid://117043192389260", name = "Attitude" },
+  } },
 }
